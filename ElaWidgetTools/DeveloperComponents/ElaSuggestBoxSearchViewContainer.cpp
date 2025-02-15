9589e67a07b596ffde69ec82e38d999ea20a4e39
ZL-Su <su-zl@seu.edu.cn>
2025-02-15 13:05:59 +0800

    Modify the default style.

--- ElaWidgetTools/DeveloperComponents/ElaSuggestBoxSearchViewContainer.cpp
+++ ElaWidgetTools/DeveloperComponents/ElaSuggestBoxSearchViewContainer.cpp
@@ -6,7 +6,7 @@
 ElaSuggestBoxSearchViewContainer::ElaSuggestBoxSearchViewContainer(QWidget* parent)
     : QWidget{parent}
 {
-    setContentsMargins(8, 8, 8, 8);
+    setContentsMargins(8, 2, 8, 8);
     setObjectName("ElaSuggestBoxSearchViewBaseWidget");
     setStyleSheet("#ElaSuggestBoxSearchViewBaseWidget{background-color:transparent}");
     _themeMode = eTheme->getThemeMode();
@@ -22,10 +22,10 @@ void ElaSuggestBoxSearchViewContainer::paintEvent(QPaintEvent* event)
     QPainter painter(this);
     painter.save();
     painter.setRenderHints(QPainter::Antialiasing);
-    eTheme->drawEffectShadow(&painter, rect(), 6, 8);
+    eTheme->drawEffectShadow(&painter, rect(), 4, 2);
     painter.setPen(ElaThemeColor(_themeMode, PopupBorder));
     painter.setBrush(ElaThemeColor(_themeMode, PopupBase));
     QRect foregroundRect(6, 0, rect().width() - 2 * 6, rect().height() - 6);
-    painter.drawRoundedRect(foregroundRect, 8, 8);
+    painter.drawRoundedRect(foregroundRect, 2, 2);
     painter.restore();
 }
