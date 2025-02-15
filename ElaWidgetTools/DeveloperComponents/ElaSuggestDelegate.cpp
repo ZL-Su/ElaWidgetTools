ddc39101fbcbc967e4c78d3958c1fabfdb28163b
ZL-Su <su-zl@seu.edu.cn>
2025-02-15 13:06:34 +0800

    Modify the default drawing style.

--- ElaWidgetTools/DeveloperComponents/ElaSuggestDelegate.cpp
+++ ElaWidgetTools/DeveloperComponents/ElaSuggestDelegate.cpp
@@ -19,7 +19,7 @@ ElaSuggestDelegate::~ElaSuggestDelegate()
 
 void ElaSuggestDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-    int margin = 2;
+    constexpr int margin = 1;
     QStyleOptionViewItem viewOption(option);
     initStyleOption(&viewOption, index);
 
@@ -34,7 +34,7 @@ void ElaSuggestDelegate::paint(QPainter* painter, const QStyleOptionViewItem& op
     QPainterPath path;
     QRect optionRect = option.rect;
     optionRect.adjust(margin * 2, margin, -margin * 2, -margin);
-    path.addRoundedRect(optionRect, 8, 8);
+    path.addRoundedRect(optionRect, 2, 2);
     if (option.state & QStyle::State_Selected)
     {
         if (option.state & QStyle::State_MouseOver)
@@ -58,7 +58,7 @@ void ElaSuggestDelegate::paint(QPainter* painter, const QStyleOptionViewItem& op
     }
     //文字绘制
     painter->setPen(ElaThemeColor(_themeMode, BasicText));
-    painter->drawText(option.rect.x() + 37, option.rect.y() + 25, suggest->getSuggestText());
+    painter->drawText(option.rect.x() + 37, option.rect.y() + 25-7, suggest->getSuggestText());
 
     //图标绘制
     if (suggest->getElaIcon() != ElaIconType::None)
@@ -66,14 +66,17 @@ void ElaSuggestDelegate::paint(QPainter* painter, const QStyleOptionViewItem& op
         QFont iconFont = QFont("ElaAwesome");
         iconFont.setPixelSize(17);
         painter->setFont(iconFont);
-        painter->drawText(option.rect.x() + 11, option.rect.y() + 26, QChar((unsigned short)suggest->getElaIcon()));
+        painter->drawText(option.rect.x() + 11, option.rect.y() + 26-7, QChar((unsigned short)suggest->getElaIcon()));
     }
     painter->restore();
 }
 
 QSize ElaSuggestDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-    QSize size = QStyledItemDelegate::sizeHint(option, index);
-    size.setHeight(40);
-    return size;
+    if (index.isValid()) {
+        QSize size = QStyledItemDelegate::sizeHint(option, index);
+        size.setHeight(25);
+        return size;
+    }
+    return{};
 }
